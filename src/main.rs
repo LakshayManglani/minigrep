7b4bcebd031a94ea62b450525e2fd5df5092dcce
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2025-02-05 13:23:54 +0530

    feat: add configuration and file reading logic
    - Add `Config` struct to handle query and file_path
    - Implement `Config::build` to parse command-line arguments
    - Implement `run` function to read and display file contents
    - Update `main.rs` to use the new `Config` and `run` logic
    - Move core logic to `lib.rs` for better code organization
    - Add `poem.txt` as a sample file for example usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, process};
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arugments: {}", err);
+        process::exit(0);
+    });
+
+    println!("> Searching for {}", config.query);
+    println!("> In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Error running program: {}", e);
+        process::exit(0);
+    }
 }
