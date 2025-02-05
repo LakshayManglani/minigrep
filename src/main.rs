7404ee9221fa0424834693238b861ba3d8afe3d8
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2025-02-05 16:10:00 +0530

    feat: add case-insensitive search and improve error handling
    - Add `ignore_case` field to `Config` to support case-insensitive search
    - Implement `search_case_insensitive` function for case-insensitive search
    - Update `run` function to use case-insensitive search based on `ignore_case` field
    - Add unit test for `search_case_insensitive` function
    - Improve error handling in `main.rs` by using `eprintln` for error messages

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arugments: {}", err);
+        eprintln!("Problem parsing arugments: {}", err);
         process::exit(0);
     });
 
-    println!("> Searching for `{}`", config.query);
-    println!("> In file `{}`", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Error running program: {}", e);
+        eprintln!("Error running program: {}", e);
         process::exit(0);
     }
 }
